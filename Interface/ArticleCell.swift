a4d7d280e448ceae291beacfc368164770123cc4
Jack Cook <hello@jackcook.nyc>
2016-04-02 14:59:02 -0400

    Implemented the article cell interface

---
+++ Interface/ArticleCell.swift
@@ -0,0 +1,22 @@
+//
+//  ArticleCell.swift
+//  Interface
+//
+//  Created by Jack Cook on 4/2/16.
+//  Copyright © 2016 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var firstLineLabel: UILabel!
+    @IBOutlet weak var sourceImage: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var articleImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ Interface/ArticlesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ArticlesViewController.swift
+//  Interface
+//
+//  Created by Jack Cook on 4/2/16.
+//  Copyright © 2016 Jack Cook. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
